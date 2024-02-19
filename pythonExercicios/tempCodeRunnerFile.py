48abe24f2eafe3739b00daa962b4fb1c45930929
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-02-19 09:48:14 -0300

    alt

--- pythonExercicios/tempCodeRunnerFile.py
+++ pythonExercicios/tempCodeRunnerFile.py
@@ -1,39 +1,12 @@
-lista  = []
-while True:
-    dic = {}
-    dic["nome"] = input("Qual seu nome: ").capitalize()
-    dic["sexo"] = input("Qual seu sexo [M/F]: ").upper().strip()[0]
-    dic["idade"] = int(input("Qual a sua idade: "))
-    lista.append(dic)
-    resposta = input("Você gostaria de continuar (S/N): ")
-    if resposta.upper() == "N":
-        break
-
-somaIdade = 0
-mulheres = []
-cadastrados = len(lista)
-
+print('=-' * 40)
+print(dic)
+print('=-' * 40)
+print(f"O jogador {dic['jogador']} jogou {partidas} partidas ")
+cont = 0
 for c in lista:
-    if c['sexo'] == 'F':
-        mulheres.append(c['nome'])
-    somaIdade += c['idade']
-
-media = somaIdade / cadastrados
-
-acimaMedia = []
-for x in lista:
-    if x['idade'] > media:
-        acimaMedia.append(x['nome'])
-
-print(f"Foram cadastradas {cadastrados} pessoas")
-print(f"As mulheres são essas: {mulheres}")
-print(f"As pessoas com idade acima da média são essas: {acimaMedia}")
-
-
-
-print(f"Foram cadastradas {cadastrados} pessoas")
-print(f"As mulheres são essas {mulheres}")
-print(f"As pessoas com idade acima da media são essas {acimaMedia}")
+    print(f"Na partida {cont +1}, fez {c} gols")
+    cont += 1
+print(f"Foi um total de {dic['total']}")
 
 
         
--- pythonExercicios/test2.py
+++ pythonExercicios/test2.py
@@ -1,18 +1,50 @@
-dic = {}
-lista = []
-dic["jogador"] = input("Qual o nome do jogador: ")
-partidas = int(input("Quantas partidas ele jogou: "))
-while partidas > len(lista):
-    gols = int(input(f"Quantos gols na partida {len(lista) + 1}: "))
-    lista.append(gols)
-dic['gols'] = lista
-dic['total'] = sum(lista)
+todos = []
+while True:
+    dic = {}
+    lista = []
+    dic["jogador"] = input("Qual o nome do jogador: ")
+    partidas = int(input("Quantas partidas ele jogou: "))
+    while partidas > len(lista):
+        gols = int(input(f"Quantos gols na partida {len(lista) + 1}: "))
+        lista.append(gols)
+    dic['gols'] = lista
+    dic['total'] = sum(lista)
+    todos.append(dic.copy())
+
+    resposta = input("Você gostaria de continuar (S/N): ")
+    if resposta.upper() == "N":
+        break
 print('=-' * 40)
-print(dic)
+print(todos)
 print('=-' * 40)
-print(f"O jogador {dic['jogador']} jogou {partidas} partidas ")
-cont = 0
-for c in lista:
-    print(f"Na partida {cont +1}, fez {c} gols")
-    cont += 1
-print(f"Foi um total de {dic['total']}")
+
+print('''
+cod  nome          gols                    total
+---------------------------------------------
+''')
+for indx, c in enumerate(todos):
+    print(f"{indx:<5}", end="")
+    for k, v in todos[indx].items():
+        if k == 'jogador':
+            print(f"{v:<15}", end="")
+        if k == 'gols':
+            print(f"{str(v):<25}", end="")
+        elif k == 'total':
+            print(v)
+print("---------------------------------------------")
+
+escolha = 80
+
+while escolha != 999: 
+    escolha = int(input("Mostrar dados de qual jogador [999 encerra]: "))
+    for c in range(len(todos)):
+        if escolha == c:
+            for k,v in todos[c].items():
+                if k == 'gols':
+                    for x,y in enumerate(v):
+                        print(f"No jogo {x} ele fez {y} gols")
+            
+
+
+
+
