347af40bd438013698669deeb0e1e4931500aa97
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-05-08 15:09:07 -0300

    alt

---
+++ OnebitCode/2-Modulos/1-modulos.py
@@ -0,0 +1,4 @@
+import calc
+
+print(calc.sum(1,2))
+print(calc.sub(1,2))
---
+++ OnebitCode/2-Modulos/modules.py
@@ -0,0 +1,13 @@
+import os
+
+#Retornar a pasta atual 
+print(os.getcwd())
+
+
+#listar arquivos e pastas 
+print(listdir())
+
+
+#listar o sistema operacional:
+os.system('ver')
+
