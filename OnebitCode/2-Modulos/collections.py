638e9e75d8aa3acdf6ca731189717bd6a71ec6b8
inteli-edu-Farias-rizk <eduardo.rizk@sou.inteli.edu.br>
2024-05-27 06:56:49 -0300

    app

--- OnebitCode/2-Modulos/collections.py
+++ OnebitCode/2-Modulos/collections.py
@@ -1,1 +1,34 @@
-import collections
+
+
+# this module is for iterating in to lists 
+
+from collections import Counter, namedtuple, deque
+from operator import itemgetter
+
+# 1 - Conta itens de uma lista
+fruits = ["Maçã", "Banana", "Uva", "Pêra", "Uva","Maçã", "Uva", "Banana"]
+print(fruits)
+print(Counter(fruits))
+
+# 2 - Utilizando uma tupla nomeada
+game = namedtuple('game', ['name', 'price', 'note'])
+g1 = game("Fifa 23", 90.50, 8.5)
+g2 = game("Resident Evil 4", 300.00, 10.0)
+print(g1)
+print(g2)
+
+# 3 - Ordenando dicionários
+students     = {"Pedro":24, "Ana":22, "Ronaldo":26, "Janaina":25}
+a = sorted(students.items(), key=itemgetter(0))
+
+print(students)
+print(a)
+
+# 4 - Utilizando fila ambas extremidades
+deq = deque([20,40,60,80])
+deq.appendleft(10)
+print(deq)
+deq.append(90)
+deq.popleft()
+deq.pop()
+print(deq)
